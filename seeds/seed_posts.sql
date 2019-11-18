5798774bcafcd07386b7c47b071a23b5e4e4a2dd
Huseyin Gundogdu <hgundogdu53@gmail.com>
2019-11-18 15:29:35 -0800

    config added

---
+++ seeds/seed_posts.sql
@@ -0,0 +1,6 @@
+INSERT INTO posts
+    (pet_name, birthdate, type_of_pet, location, hobbies, owner)
+VALUES
+    ('Muffin', '2019', 'Cat', 'New York', 'White cat that loves playing with small cats. Friendly and lovely.', 1),
+    ('SuperJack', '2019', 'Shepherd', 'San Francisco', 'Small shephers that like all kind of dogs. He is very good at baseball.', 1),
+    ('Greeny', '2019', 'Iguana', 'Virginia', 'Greeny likes best are climbing and swimming and having fun in water. You can start by letting climb up your body and also playing with him while he is having a bath.')
