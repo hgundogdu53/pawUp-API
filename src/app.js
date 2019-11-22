1005535b2e22085ee65ea83a324b3f25f64eadab
Huseyin Gundogdu <hgundogdu53@gmail.com>
2019-11-21 20:50:09 -0800

    error handler

--- src/app.js
+++ src/app.js
@@ -30,12 +30,14 @@ app.get('/', (req, res) => {
 
 app.use(function errorHandler(error, req, res, next) {
     let response
-    if (NODE_ENV === 'production') {
+    /*if (NODE_ENV === 'production') {
         response = { error: { message: 'server error' } }
     } else {
         console.error(error)
         response = { message: error.message, error }
-    }
+    }*/
+    console.error(error)
+    response = { message: error.message, error }
     res.status(500).json(response)
 })
 
