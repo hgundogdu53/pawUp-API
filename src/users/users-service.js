87a398b5b57e31e316c2a59d0c29214ea6b94359
Huseyin Gundogdu <hgundogdu53@gmail.com>
2019-11-21 20:55:16 -0800

    hasuseremail

--- src/users/users-service.js
+++ src/users/users-service.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcryptjs')
 const REGEX_UPPER_LOWER_NUMBER_SPECIAL = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/
 
 const UsersService = {
-    hasUserWithUserEmail(db, email) {
+    hasUserWithEmail(db, email) {
         return db('users')
             .where({ email })
             .first()
